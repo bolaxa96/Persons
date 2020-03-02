50f26b1d7670621ed0762bd22a7289f5bc6ba43b
Pedro Nuno Dias Rodrigues <pedrocampia@hotmail.com>
2020-03-02 21:31:16 +0000

    1ºone

---
+++ WindowsFormsApp1/Pessoa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    class Pessoa
+    {
+        private string Nome;
+        private int idade;
+        private int id;
+        public Pessoa(int id,string nome, int idade)
+        {
+            this.Nome = nome;
+            this.idade = idade;
+            this.id=id;
+        }
+        public int getage()
+        {
+     
+             return idade;
+        }
+        public string getnome()
+        {
+
+            return Nome;
+        }
+        public int getid()
+        {
+
+            return id;
+        }
+        
+
+    }
+}
